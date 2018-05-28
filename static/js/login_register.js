0b8f84e63862e7ddd42a0e6509ad30c8504d2fe4
Milan Kiszely <milankiszely@gmail.com>
2018-05-28 14:53:05 +0200

    login commitn for merging

--- static/js/login_register.js
+++ static/js/login_register.js
@@ -1,8 +1,28 @@
 function loadEventListeners() {
     // Here comes the login and register form submitting with ajax.
+    document.getElementById('login').addEventListener('click', login, false);
 
 }
 
-$(document).ready(function() {
+function login (e) {
+    e.preventDefault();
+    let username = e.target.form[0].value;
+    let password = e.target.form[1].value;
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/login');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            alert(xhr.response);
+        }
+        else if (xhr.status !== 200) {
+            alert('Request failed.  Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send(encodeURI('username='+username+'&password='+password))
+
+}
+
+window.onload = function() {
     loadEventListeners();
-});
+};
