d497957f828e219dd47c21c8419bc54e6a181f49
mogyorosibalin <mogyorosibalin@gmail.com>
2018-05-28 13:56:28 +0200

    basic register routing with ajax created

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, jsonify
 app = Flask(__name__)
 
 app.secret_key = "some_random_secret_key_for_proman_sprint_2"
@@ -10,6 +10,12 @@ def boards():
     if 'user' not in session:
         return render_template('user.html')
     return render_template('boards.html')
+
+
+@app.route("/register", methods=['POST'])
+def register():
+    print(request.form.to_dict())
+    return jsonify({'success': True})
 
 
 def main():
--- static/js/login_register.js
+++ static/js/login_register.js
@@ -1,6 +1,21 @@
 function loadEventListeners() {
     // Here comes the login and register form submitting with ajax.
-
+    $('#registerForm').on('submit', function(event) {
+        event.preventDefault();
+        $.ajax({
+            type: 'post',
+            url: '/register',
+            data: {
+                username: $(this).find('input[name=username]').val(),
+                password: $(this).find('input[name=password]').val(),
+                passwordAgain: $(this).find('input[name=passwordAgain]').val()
+            },
+            dataType: 'json',
+            success: function(data) {
+                // console.log(data);
+            }
+        });
+    });
 }
 
 $(document).ready(function() {
